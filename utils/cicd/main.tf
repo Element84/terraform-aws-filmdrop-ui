ab1b3d81a9e6b10f08f2e97c4bf52e0da8ef0985
Theodore Reuter <theodore.reuter@gmail.com>
2025-12-15 15:50:32 -0500

    feat: tweak to pass file contents of config and logo files

--- utils/cicd/main.tf
+++ utils/cicd/main.tf
@@ -11,8 +11,8 @@ module "vpc-data" {
 module "main" {
   source                  = "../.."
   filmdrop_ui_logo_file   = "../../logo.png"
-  filmdrop_ui_config      = "../../config.dev.json"
-  filmdrop_ui_logo        = "bm9uZQo=" # Base64: 'none'
+  filmdrop_ui_config      = filebase64("../../config.dev.json")
+  filmdrop_ui_logo        = filebase64("../../logo.png")
   filmdrop_ui_release_tag = "v6.1.1-0"
   vpc_id                  = module.vpc-data.vpc_id
   vpc_private_subnet_ids  = module.vpc-data.private_subnet_ids
